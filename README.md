f79fa718fb5a919366072f697199a4a0ac3cd2c4
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 <royx520@gmail.com>
2022-12-29 18:31:45 +0200

    update readme

--- README.md
+++ README.md
@@ -25,9 +25,42 @@ should receive updates on any changes made to the database.
 
 In GroupAdmin class we use Array list for save all the member.
 
+GroupAdmin is a class that Contains a list of members who can register or unregister from the group
+and holds a pointer to UndoableStringBuilder in a shallow copy so all the member get the same UndoableStringBuilder.
+
+The class has several methods that allow change in the UndoableStringBuilder so that all the members see the same change because we work with shallow copy.
+
 ### ConcreteMember
 ConcreteMember implements the Member interface
 which describes the recipient of the updates.
 
 The class contains within it the copy of the UndoableStringBuilder repository.
 
+In class ConcreteMember we create members who can register for Groupadmin,
+
+In addition, it has the update method that allows the member to point to the UndoableStringBuilder of the groupadmin.
+
+### Observer pattern
+In this assignment we worked with Observer pattern. 
+
+Observer is design pattern which an object, named the Observable,
+maintains a list of its dependents (Observer) , called observers, 
+and notifies them automatically of any state changes by the method notifyMembers.
+
+In our assignment Observer is the member of ConcreteMember
+and the Observable is the GroupAdmin.
+
+## Tests
+
+The tests were written using JUnit.
+
+### GroupAdmin tests
+For this class we write tests for register method  and unregister method
+to test edge cases.
+
+
+### ConcreteMember tests
+For this class we write tests for update.
+
+### JvmUtilities tests
+In each test we write, we also added a JvmUtilities test to check the memory efficiency of the classes we built.
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Tests {
     public static final Logger logger = LoggerFactory.getLogger(Tests.class);
+
     // stub method to check external dependencies compatibility
     @Test
     public void test(){
@@ -30,18 +31,22 @@ public class Tests {
         ConcreteMember member1 = new ConcreteMember("roy");
         ConcreteMember member2 = new ConcreteMember("yuval");
 
-        groupAdmin.register(member1);
+        groupAdmin.register(member1); //register  2 member
         groupAdmin.register(member2);
         assertTrue(groupAdmin.getMembers().contains(member1));
         assertTrue(groupAdmin.getMembers().contains(member2));
+        logger.info(()->JvmUtilities.objectFootprint(groupAdmin));
 
-        groupAdmin.register(null);
+        groupAdmin.register(null); //try register member that point to null
         assertEquals(2, groupAdmin.getMembers().size());
+        logger.info(()->JvmUtilities.objectFootprint(groupAdmin));
 
-        groupAdmin.register(member1);
+
+        groupAdmin.register(member1);  //try register member that already exists in groupadmin
         assertEquals(2, groupAdmin.getMembers().size());
         assertTrue(groupAdmin.getMembers().contains(member1));
         assertTrue(groupAdmin.getMembers().contains(member2));
+        logger.info(()->JvmUtilities.objectFootprint(groupAdmin));
 
     }
 
@@ -51,46 +56,56 @@ public class Tests {
         ConcreteMember member1 = new ConcreteMember("roy");
         ConcreteMember member2 = new ConcreteMember("yuval");
 
-        groupAdmin.register(member1);
+        groupAdmin.register(member1); //register  2 member and unregister one of them
         groupAdmin.register(member2);
         groupAdmin.unregister(member1);
         assertFalse(groupAdmin.getMembers().contains(member1));
         assertTrue(groupAdmin.getMembers().contains(member2));
+        logger.info(()->JvmUtilities.objectFootprint(groupAdmin));
 
-        groupAdmin.unregister(null);
+
+        groupAdmin.unregister(null); //try unregister member that point to null
         assertEquals(1, groupAdmin.getMembers().size());
+        logger.info(()->JvmUtilities.objectFootprint(groupAdmin));
 
-        ConcreteMember member4 = new ConcreteMember("noam");
+
+        ConcreteMember member4 = new ConcreteMember("noam"); //try unregister member that not in groupadmin
         groupAdmin.unregister(member4);
         assertEquals(1, groupAdmin.getMembers().size());
+        logger.info(()->JvmUtilities.objectFootprint(groupAdmin));
 
-        groupAdmin.unregister(member2);
+
+        groupAdmin.unregister(member2); //try unregister member and let the groupadmin to be in size 0
         assertEquals(0, groupAdmin.getMembers().size());
+        logger.info(()->JvmUtilities.objectFootprint(groupAdmin));
+
     }
 
-    @Test
-    void testupdate() {
-        GroupAdmin groupAdmin = new GroupAdmin();
-        ConcreteMember member1=new ConcreteMember("roy");
-        groupAdmin.register(member1);
-        groupAdmin.insert(0,"test");
-
-        ConcreteMember member2 =new ConcreteMember("yuval");
-        groupAdmin.register(member2);
-        assertNotEquals(member1.getMemberusb().toString(),member2.getMemberusb().toString());
-        groupAdmin.append(" update");
-        assertEquals(member1.getMemberusb().toString(),member2.getMemberusb().toString());
-    }
     @Test
     void update() {
-        ConcreteMember member = new ConcreteMember("roy");
+
+        //check if after change in usb the member get the change
+        ConcreteMember member1 = new ConcreteMember("roy");
         ConcreteMember member2 = null;
         UndoableStringBuilder usb = new UndoableStringBuilder();
-        member.update(usb);
+        member1.update(usb);
         usb.append("Hello");
-        assertEquals("Hello", member.getMemberusb().toString());
+        assertEquals("Hello", member1.getMemberusb().toString());
         usb.append(" world!");
-        assertEquals("Hello world!", member.getMemberusb().toString());
+        assertEquals("Hello world!", member1.getMemberusb().toString());
+        logger.info(()->JvmUtilities.objectFootprint(member1));
+
+
+        //check if after add new member to groupadmin he get the usb after update
+        GroupAdmin groupAdmin = new GroupAdmin();
+        groupAdmin.register(member1);
+        groupAdmin.insert(0,"test");
+        ConcreteMember member3 =new ConcreteMember("yuval");
+        groupAdmin.register(member3);
+        assertNotEquals(member1.getMemberusb().toString(),member3.getMemberusb().toString());
+        groupAdmin.append(" update");
+        assertEquals(member1.getMemberusb().toString(),member3.getMemberusb().toString());
+        logger.info(()->JvmUtilities.objectFootprint(groupAdmin));
 
     }
 }
